4c07b886b322a2c258ab02af9a72706bc534dfb9
Npontiroli21 <npontiroli@estudiantes.unsam.edu.ar>
2025-06-06 12:08:51 -0300

    Agrego OperationTimer.java

--- unsam_tp_final/src/utils/OperationTimer.java
+++ unsam_tp_final/src/utils/OperationTimer.java
@@ -1,5 +1,29 @@
 // OperationTimer.java - clase o interfaz de utils
 package utils;
 
+public class OperationTimer {
+    private long startTime;
+    private long endTime;
 
+    public void start() {
+        startTime = System.nanoTime();
+    }
+
+    public void stop() {
+        endTime = System.nanoTime();
+    }
+
+    public long getElapsedTime() {
+        return endTime - startTime;
+    }
+
+    public double getElapsedSeconds() {
+        return (endTime - startTime) / 1_000_000_000.0;
+    }
+
+    public String getFormattedElapsedTime() {
+        long elapsedNanos = getElapsedTime();
+        return String.format("%d ms", elapsedNanos / 1_000_000);
+    }
+}
 
